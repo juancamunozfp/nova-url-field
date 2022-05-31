938cc5f0866cc3801b012f067e616b1d3077e3b1
Juan Carlos Muñoz Cabrera <jcmunoz@freepik.com>
2022-05-31 13:17:24 +0200

    nova 4

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,10 @@
 let mix = require('laravel-mix');
 let path = require('path');
 
+require('./nova.mix.js');
 
-mix.js('resources/js/field.js', 'dist/js').vue({ version: 3 })
-    .alias({
-        'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
-    })
-    .webpackConfig({
-        externals: {
-            vue: 'Vue',
-        },
-        output: {
-            uniqueName: 'inspheric/nova-url-field',
-        },
-    })
+mix
+    .setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .vue({ version: 3 })
+    .nova('inspheric/nova-url-field')
